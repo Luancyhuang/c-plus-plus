bec496f87bd2b2c24c9dc7ca81386b3925fab602
Luancyhuang <542914825@qq.com>
2022-04-20 07:00:55 -0700

    priority_queue-优先级队列的接口函数练习

--- test.cc
+++ test.cc
@@ -1,22 +1,38 @@
 #include <iostream>
+using namespace std;
+
+#include <list>
 #include <vector>
+#include <queue>
+#include <functional>
+
+void test_priority_queue()
+{
+  //默认是大的优先级高--大堆，默认给的仿函数是less,
+  //priority_queue<int> pq;
+  
+  //控制小的优先级高  --小堆，给的是greater防函数
+  priority_queue< int,vector<int>,greater<int> > pq;
+  
+  pq.push(3);
+  pq.push(3);
+  pq.push(7);
+  pq.push(1);
+  pq.push(9);
+  
+  while(!pq.empty())
+  {
+    cout << pq.top() << " ";
+    pq.pop();
+  }
+  cout << endl;
+}
 
 int main()
-{
-  size_t sz;
-  std:: vector<int> foo;
-  sz = foo.capacity();
-  std:: cout << "making foo grow:\n";
-
-  for(int  i = 0; i < 100; ++i)
-  {
-    foo.push_back(i);
-    if(sz != foo.capacity())
-    {
-      sz = foo.capacity();
-      std:: cout << "capacity changed:" << sz << '\n';
-    }
-  }
+{    
+  test_priority_queue();
+  
   return 0;
 }
+
 
